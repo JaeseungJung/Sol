f928544f45600e0574409bb3825bc99e552f49c1
JaeseungJung <86044304+JaeseungJung@users.noreply.github.com>
2021-07-27 11:24:16 +0900

    [UPDATE]

--- first.sol
+++ first.sol
@@ -14,8 +14,19 @@ contract FirstContract {
     fallback() external payable {
       
     }
+    
+   uint age = 19;
+    
+    function getAge() public view returns (uint) {
+        return age;
+    }
+
+    function setAge(uint _age) public {
+        age = _age;
+    }
    
 }
+
 
 // fallback~ 스마트컨트랙트에 이더를 입금하게 하는 코드
 
