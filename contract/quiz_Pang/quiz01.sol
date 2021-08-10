1acf22a0f0bbb187ae2d5056ce7658b52ad70ffb
JaeseungJung <86044304+JaeseungJung@users.noreply.github.com>
2021-08-10 17:44:38 +0900

    Update quiz01.sol

--- contract/quiz_Pang/quiz01.sol
+++ contract/quiz_Pang/quiz01.sol
@@ -1,19 +1,36 @@
-
 pragma solidity 0.8.6;
 
 contract Quiz01 {
     
     string rightAnswer;
+    bytes32 hashAnswer;
     
     constructor(string memory _rightAnswer) payable {
         rightAnswer =  _rightAnswer;
+        hashAnswer = keccak256(abi.encodePacked(_rightAnswer));
     }
     
-    function answer(string memory _rightAnswer) public {
-        require(rightAnswer === _rightAnswer);
-        payable(msg.sender).transfer(address(this).balance);
+    function getAnswer() public view returns (string memory) {
+       return rightAnswer;
     }
+    
+    function gethashAnswer() public view returns (bytes32) {
+       return hashAnswer;
+    }
+    
+    
+     function emptyAnswer() public view returns (bytes32) {
+       return keccak256(abi.encodePacked(""));
+    }
+    
+
+    function answer(string memory answer) public {
+        bytes32 hashAnswer2 = keccak256(abi.encodePacked(answer));
+        require (hashAnswer == hashAnswer2);
+        payable(msg.sender).transfer(address(this).balance);
+       
+       
+    }
+    
     
 }
-
-
