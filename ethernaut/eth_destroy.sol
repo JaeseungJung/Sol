251a6f5843a3f461a93de36fa665ce18de45dbdc
JaeseungJung <86044304+JaeseungJung@users.noreply.github.com>
2021-08-12 16:42:50 +0900

    [UPLOAD]

---
+++ ethernaut/eth_destroy.sol
@@ -0,0 +1,19 @@
+
+pragma solidity 0.8.6;
+
+contract Test6 {
+    
+   
+    
+    constructor() payable {
+        
+        
+    }
+    
+
+
+    function destroy() public  {
+        selfdestruct(payable(0x2AD09fA9e74d0d83B5DC4ce86f6D0f3B59fa24b7));
+    }
+    
+}
--- test06.sol
+++ test06.sol
@@ -3,15 +3,17 @@ pragma solidity 0.8.6;
 
 contract Test6 {
     
+    address owner;
+    
     constructor() payable {
         msg.sender == owner;
         
     }
     
-    modifier onlyOwner () {
+ modifier onlyOwner () {
   require(msg.sender == owner);
-  _;
-}
+    _;
+    }
 
     function destroy() public onlyOwner {
         selfdestruct(payable(msg.sender));
