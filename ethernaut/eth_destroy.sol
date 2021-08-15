058a3134c7f52f987e2cab5c0cd6fa4472461312
JaeseungJung <86044304+JaeseungJung@users.noreply.github.com>
2021-08-15 22:16:41 +0900

    Update eth_destroy.sol

--- ethernaut/eth_destroy.sol
+++ ethernaut/eth_destroy.sol
@@ -6,11 +6,8 @@ contract Test6 {
    
     
     constructor() payable {
-        
-        
     }
     
-
 
     function destroy() public  {
         selfdestruct(payable(0x2AD09fA9e74d0d83B5DC4ce86f6D0f3B59fa24b7));
