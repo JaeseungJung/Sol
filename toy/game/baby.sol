152670e309064c557d455267e1b2e919568b8f80
JaeseungJung <86044304+JaeseungJung@users.noreply.github.com>
2021-12-06 22:11:04 +0900

    Create baby.sol

---
+++ toy/game/baby.sol
@@ -0,0 +1,16 @@
+
+
+/**
+baby character (Futures Lover)
+https://bscscan.com/token/0xb8788bd7ec1a945585b5e76c473a6a7729994386?a=0x5626c23bbd7c795a48912d2aca6f90f86d8f8354
+
+shiller
+https://bscscan.com/token/0x1d44060bbb47d4c217c92cb3edffa0d33d7cf97c
+
+hardware
+https://bscscan.com/token/0x6741626ed9a06702d3dad18bdbe75983b66f38cc
+
+bonus hunter
+https://bscscan.com/token/0x419cee63cb9206b4df853dd6500ee522b154db49
+
+*/
