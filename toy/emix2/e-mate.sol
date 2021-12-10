ca1b31c39ea1e1bd276f96d6d622fa824fc63cd4
JaeseungJung <86044304+JaeseungJung@users.noreply.github.com>
2021-12-11 00:57:48 +0900

    Update e-mate.sol

--- toy/emix2/e-mate.sol
+++ toy/emix2/e-mate.sol
@@ -2,8 +2,10 @@
 
 https://etherscan.io/token/0xd0242443f18586c389a1013539e93f3a7b27018c#writeContract
 
+Emix
+https://etherscan.io/token/0x5db69b9f173f9d9fa91b7cdcc4dc9939c0499cfe
 
-token URI
+NFT URI
 https://api.dogesound.club/emates/1
 https://storage.googleapis.com/emates/nft/Emates-353.png
 
