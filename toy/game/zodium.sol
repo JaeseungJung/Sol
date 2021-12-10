c8fda63514053196e8cd2daa63afd7e07cb1f35a
JaeseungJung <jason3742@gmail.com>
2021-12-10 17:48:47 +0900

    Update zodium.sol

--- toy/game/zodium.sol
+++ toy/game/zodium.sol
@@ -8,4 +8,8 @@ https://bscscan.com/token/0x0cca2f5561bb0fca88e5b9b48b7fbf000349c357
 Zodium12KINDs
 https://bscscan.com/token/0x4c4bd203b7bb3a06825cad642df9f43278eb24c7
 
+supply: 11,151  / 12-10-17-41
+3582 remaining
+
+
  */
