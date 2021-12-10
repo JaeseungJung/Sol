d5026cfc0707a08abf7669b26d4dedc656d72501
JaeseungJung <jason3742@gmail.com>
2021-12-10 17:40:55 +0900

    Create zodium.sol

---
+++ toy/game/zodium.sol
@@ -0,0 +1,11 @@
+/**
+
+Zodi Token
+https://bscscan.com/token/0x0cca2f5561bb0fca88e5b9b48b7fbf000349c357
+
+
+
+Zodium12KINDs
+https://bscscan.com/token/0x4c4bd203b7bb3a06825cad642df9f43278eb24c7
+
+ */
